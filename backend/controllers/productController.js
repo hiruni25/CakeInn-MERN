973e9d7915e7db961f0e18e26e10d53c9189ddf7
hiruni25 <hirudesilva25@gmail.com>
2021-08-25 23:01:37 +0530

    Implemented Middleware to Error Handling

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,7 @@
 const Product = require('../models/product')
 
+const ErrorHandler = require('../utils/errorHandler');
+
 // Create new product => /api/v1/admin/product/new
 exports.newProduct = async (req, res, next) => {
 
@@ -29,10 +31,7 @@ exports.getSingleProduct = async (req, res, next) => {
     const product = await Product.findById(req.params.id);
 
     if (!product){
-        return res.status(404).json({
-            success: false,
-            message: 'Product not found'
-        })
+        return next(new ErrorHandler('Product not found',404));
     }
 
     res.status(200).json({
