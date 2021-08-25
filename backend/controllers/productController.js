3be8bcfd297ac14925160fc4b6cb1150440193dd
hiruni25 <hirudesilva25@gmail.com>
2021-08-25 17:13:20 +0530

    Added Update Product function

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,6 @@
 const Product = require('../models/product')
 
-// Create new product => /api/v1/product/new
+// Create new product => /api/v1/admin/product/new
 exports.newProduct = async (req, res, next) => {
 
     const product = await Product.create(req.body);
@@ -34,6 +34,30 @@ exports.getSingleProduct = async (req, res, next) => {
             message: 'Product not found'
         })
     }
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+// Update Product => /api/v1/admin/product/:id
+exports.updateProduct = async (req, res, next) => {
+
+    let product = await Product.findById(req.params.id);
+
+    if (!product){
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
 
     res.status(200).json({
         success: true,
