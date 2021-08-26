a8b74e437c102ab107041851fe098c67d5a2d9b9
hiruni25 <hirudesilva25@gmail.com>
2021-08-27 00:17:25 +0530

    Uncaught Exceptions, Validation & Mongose ID Error

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,16 @@ const connectDatabase = require('./config/database')
 
 const dotenv = require('dotenv');
 
+// Handle Uncaught Exceptions
+process.on('uncaughtException', err => {
+    console.log(`ERROR: ${err.stack}`);
+    console.log('Shutting down due to Uncaught Exception');
+    process.exit(1)
+})
+
 // Setting up config file
 dotenv.config({path:'backend/config/config.env'})
+
 
 // Connecting to database
 connectDatabase();
@@ -16,7 +24,7 @@ const server = app.listen(process.env.PORT, () => {
 // Handle Unhandled Promise Rejections
 process.on('unhandledRejection', err => {
     console.log(`ERROR: ${err.message}`);
-    console.log(`Shutting down the server due to the Unhandled Promise Rejection`);
+    console.log(`Shutting down the server due to Unhandled Promise Rejection`);
     server.close(() => {
         process.exit(1)
     })
