adad93b817bbd7cd624c06a5295ca93e14ff08a8
hiruni25 <hirudesilva25@gmail.com>
2021-08-26 15:23:18 +0530

    Added changes : Development Vs Production Errors

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -2,10 +2,26 @@ const ErrorHandler = require('../utils/errorHandler');
 
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
-    err.message = err.message || 'Internal Server Error';
+    
+    if(process.env.NODE_ENV === 'DEVELOPMENT'){
+        res.status(err.statusCode).json({
+            success: false,
+            error: err,
+            errMessage: err.message,
+            stack: err.stack
+        })
+    }
 
-    res.status(err.statusCode).json({
-        success: false,
-        error: err.stack
-    })
+    if(process.env.NODE_ENV === 'PRODUCTION'){
+        let error = { ...err }
+
+        error.message = err.message
+
+        res.status(error.statusCode).json({
+            success: false,
+            message: error.message || 'Internal Server Error'
+        })
+    }
+
+    
 }
