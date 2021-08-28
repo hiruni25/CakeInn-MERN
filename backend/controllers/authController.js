3e9ffbc7cf5a888b446ca58ee73fe63de1ec7262
hiruni25 <hirudesilva25@gmail.com>
2021-08-28 23:41:40 +0530

    Implemented User Login & Assign Token functions

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -25,3 +25,34 @@ exports.registerUser = catchAsyncErrors( async (req, res, next) => {
         token
     })
 })
+
+// Login User => /api/v1/login
+exports.loginUser = catchAsyncErrors( async(req, res, next) => {
+    const { email, password } = req.body;
+
+    // Check if email and password is entered by user
+    if(!email || !password) {
+        return next(new ErrorHandler('Please enter email & password', 400))
+    }
+
+    // Finding user in database
+    const user = await User.findOne({ email }).select('+password')
+
+    if(!user) {
+        return next(new ErrorHandler('Invalid email or password', 401));
+    }
+
+    // Checks if password is correct or not
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if(!isPasswordMatched) {
+        return next(new ErrorHandler('Invalid email or password', 401));
+    }
+
+    const token = user.getJwtToken();
+
+    res.status(200).json({
+        success: true,
+        token
+    })
+})
