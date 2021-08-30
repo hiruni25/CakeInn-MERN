9c1621ee1ce990ba2722721366f388b411c6da8b
hiruni25 <hirudesilva25@gmail.com>
2021-08-30 23:31:16 +0530

    Authorize User Roles and Permissions

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -17,3 +17,14 @@ exports.isAuthenticatedUser = catchAsyncErrors( async (req, res, next) => {
     next()
 
 })
+
+// Handling User Roles
+exports.authorizeRoles = (...roles) => {
+    return (req, res, next) => {
+        if(!roles.includes(req.user.role)) {
+            return next(
+            new ErrorHandler(`Role (${req.user.role}) is not allowed to access this resource`, 403))
+        }
+        next()
+    }
+}
