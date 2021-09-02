0e86f8ed03f9e30e15fab8ee8f2e7efcc985cf35
hiruni25 <hirudesilva25@gmail.com>
2021-09-02 23:47:34 +0530

    Show User Profile

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -126,6 +126,16 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
     sendToken(user, 200, res)
 })
 
+// Get currently logged in user details => /api/v1/profile
+exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json({
+        success: true,
+        user
+    })
+})
+
 // Logout User => /api/v1/logout
 exports.logout = catchAsyncErrors(async (req, res, next) => {
     res.cookie('token', null, {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser, loginUser, forgotPassword, resetPassword, logout } = require('../controllers/authController');
+const { 
+    registerUser, 
+    loginUser, 
+    forgotPassword, 
+    resetPassword, 
+    logout, 
+    getUserProfile} = require('../controllers/authController');
+
+const { isAuthenticatedUser } = require('../middlewares/auth');
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
@@ -10,5 +18,7 @@ router.route('/password/forgot').post(forgotPassword);
 router.route('/password/reset/:token').put(resetPassword);
 
 router.route('/logout').get(logout);
+
+router.route('/profile').get(isAuthenticatedUser, getUserProfile);
 
 module.exports = router;
