ac01a053a148f87439f882610472f8c6fc04ff3b
hiruni25 <hirudesilva25@gmail.com>
2021-09-03 13:21:09 +0530

    Admin - Get All Users & Specific User Details

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -9,9 +9,11 @@ const {
     logout, 
     getUserProfile,
     updatePassword,
-    updateProfile} = require('../controllers/authController');
+    updateProfile,
+    allUsers,
+    getUserDetails} = require('../controllers/authController');
 
-const { isAuthenticatedUser } = require('../middlewares/auth');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
@@ -24,5 +26,8 @@ router.route('/logout').get(logout);
 router.route('/profile').get(isAuthenticatedUser, getUserProfile);
 router.route('/password/update').put(isAuthenticatedUser, updatePassword);
 router.route('/profile/update').put(isAuthenticatedUser, updateProfile);
+
+router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'), allUsers);
+router.route('/admin/user/:id').get(isAuthenticatedUser, authorizeRoles('admin'), getUserDetails);
 
 module.exports = router;
