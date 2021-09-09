b76e8d43527ccf0e6bf99fb413ad45f21be2a58c
hiruni25 <hirudesilva25@gmail.com>
2021-09-10 02:02:14 +0530

    Get All Reviews of Product

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -7,7 +7,8 @@ const {
     getSingleProduct, 
     updateProduct, 
     deleteProduct,
-    createProductReview
+    createProductReview,
+    getProductReviews
 } = require('../controllers/productController')
 
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
@@ -23,5 +24,6 @@ router.route('/admin/product/:id')
             .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
 
 router.route('/review').put(isAuthenticatedUser, createProductReview)
+router.route('/reviews').get(isAuthenticatedUser, getProductReviews)
 
 module.exports = router;
