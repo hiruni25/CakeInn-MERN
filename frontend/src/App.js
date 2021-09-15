85dca3e216e91c2bf8be236c24c49103f756ac28
hiruni25 <hirudesilva25@gmail.com>
2021-09-16 02:50:13 +0530

    Added Home Page Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 //import './App.css'
-import Header from './components/layout/Header'
+import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       <Footer />
     </div>
   );
