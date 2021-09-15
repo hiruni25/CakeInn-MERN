c0141105791883be67da096fe61aa6831f69eb1b
hiruni25 <hirudesilva25@gmail.com>
2021-09-15 23:57:02 +0530

    Added Header & Footer Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+//import './App.css'
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>CakeInn</h1>
+      <Header />
+      <Footer />
     </div>
   );
 }
