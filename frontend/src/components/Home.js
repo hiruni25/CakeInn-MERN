4d318db3f7ce08c93cff09401ce143fe3f851fde
hiruni25 <hirudesilva25@gmail.com>
2021-09-19 20:43:55 +0530

    Added Loader Component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from 'react'
 
 import MetaData from './layout/MetaData'
 import Product from './product/Product'
+import Loader from './layout/Loader'
 
 import { useDispatch, useSelector } from 'react-redux'
 import { getProducts } from '../actions/productActions'
@@ -18,24 +19,24 @@ const Home = () => {
 
     return (
         <Fragment>
-            {loading ? <h1>Loading...</h1> : (
-            <Fragment>
-                <MetaData title={'Buy Best Products'} />
-                
-                <h1 id="products_heading">Latest Products</h1>
+            {loading ? <Loader /> : (
+                <Fragment>
+                    <MetaData title={'Buy Best Products'} />
 
-                <section id="products" className="container mt-5">
-                    <div className="row">
+                    <h1 id="products_heading">Latest Products</h1>
 
-                        {products && products.map(product => (
+                    <section id="products" className="container mt-5">
+                        <div className="row">
 
-                            <Product key={product._id} product={product} />
+                            {products && products.map(product => (
 
-                        ))}
+                                <Product key={product._id} product={product} />
 
-                    </div>
-                </section>
-            </Fragment>
+                            ))}
+
+                        </div>
+                    </section>
+                </Fragment>
             )}
 
         </Fragment>
