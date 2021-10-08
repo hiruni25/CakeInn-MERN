22031955213c2ecc72536240419f858581349fe3
hiruni25 <hirudesilva25@gmail.com>
2021-10-08 21:08:26 +0530

    Added Cloudinary for Images

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,19 @@ const app = express();
 const cookieParser = require('cookie-parser');
 
 const errorMiddleware = require('./middlewares/errors')
+const bodyparser = require('body-parser')
+const cloudinary = require('cloudinary')
 
 app.use(express.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// Setting up cloudinary configuration
+cloudinary.config({
+    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+    api_key : process.env.CLOUDINARY_API_KEY,
+    api_secret : process.env.CLOUDINARY_API_SECRET
+})
 
 // Import all routes
 const products = require('./routes/product');
